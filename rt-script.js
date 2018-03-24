703913c5c923787a1b84a696f23dba2aaca36762
Doug A Carter <doug.allen.carter@gmail.com>
2018-03-24 19:25:59 -0400

    Added comments
    Also attempted a fix for mobile responsiveness.

--- rt-script.js
+++ rt-script.js
@@ -1,41 +1,60 @@
-        var start = new Date().getTime();
-        var highScore = [];
-        
-        function getRandomColor() {
-            var letters = '0123456789ABCDEF'.split('');
-            var color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-        function makeShapeAppear(){
-            document.getElementById("shape").style.display = "block";
-            start = new Date().getTime();
-        }
-        function appearAfterDelay(){
-            var top = Math.random() * 500;
-            var left = Math.random() * 1000;
-            var width = (Math.random() * 200) + 100;
-
-            if (Math.random() >.5){    
-            document.getElementById("shape").style.borderRadius = "50%";
-            } else{
-                document.getElementById("shape").style.borderRadius = "0";
-            }
-            document.getElementById("shape").style.backgroundColor = getRandomColor();   
-            document.getElementById("shape").style.top= top + "px";
-            document.getElementById("shape").style.left= left + "px";
-            document.getElementById("shape").style.width = width + "px";
-            document.getElementById("shape").style.height = width + "px";
-            setTimeout(makeShapeAppear, Math.random() * 2000);
-        
-        }
-        appearAfterDelay();
-          
-        document.getElementById("shape").onclick = function(){
-            document.getElementById("shape").style.display = "none";
+      /*Timer*/            
+            var start = new Date().getTime();
+/*High score array*/       
+            var highScore = [];
             
+            alert(window.innerHeight);
+            
+/*Set random color*/    
+            
+            function getRandomColor() {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
+/*Set random variables to determain shape, size, location*/            
+            function makeShapeAppear() {
+                
+                var top =  Math.random() * (window.innerHeight *.55); 
+                var left = Math.random() * (window.innerWidth*.65);
+                var width = (Math.random() * 100) + 50;
+                
+                if (Math.random() > 0.5) {
+/*Make it a circle*/                    
+                    document.getElementById("shape").style.borderRadius = "50%";
+                    
+                } else {
+/*Make it a square*/                    
+                    document.getElementById("shape").style.borderRadius = "0";
+                    
+                }
+/*Create random size  shape*/                
+                document.getElementById("shape").style.backgroundColor = getRandomColor();
+                document.getElementById("shape").style.width = width + "px";
+                document.getElementById("shape").style.height = width + "px";
+                document.getElementById("shape").style.top = top + "px";
+                document.getElementById("shape").style.left = left + "px";
+                document.getElementById("shape").style.display = "block";
+               
+                start = new Date().getTime();
+
+            }
+/*Make a new shape pop up at random time*/            
+            function appearAfterDelay() {
+                setTimeout(makeShapeAppear, Math.random() * 2000);
+                
+            }
+            appearAfterDelay();
+
+            
+/*Dissapear on click*/          
+            document.getElementById("shape").onclick = function(){
+                document.getElementById("shape").style.display = "none";
+            
+/*Scoring*/            
             var end = new Date().getTime();
             var timeTaken = (end - start) / 1000;
             highScore.push(timeTaken);
